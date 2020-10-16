dfb706903a58ec9c70d62efd2d9ca12c4b5a2cc1
Matthew Lawson <mmlawson@ucdavis.edu>
2020-10-16 16:08:12 +0200

    got field shit working

--- mk_ns_field_plot.py
+++ mk_ns_field_plot.py
@@ -8,37 +8,20 @@ import ns_field
 
 ns = ns_field.ns()
 
-# Mean magnitude of the Earth's magnetic field at the equator in T
-B0 = 3.12e-5
-# Radius of Earth, Mm (10^6 m: mega-metres!)
-RE = 6.370
-# Deviation of magnetic pole from axis
-# alpha = np.radians(9.6)
-
-def B(r, theta, alpha):
-    """Return the magnetic field vector at (r, theta)."""
-    fac = B0 * (RE / r)**3
-    return -2 * fac * np.cos(theta + alpha), -fac * np.sin(theta + alpha)
-
 fig, ax = plt.subplots()
 
 # Grid of x, y points on a Cartesian grid
 nx, ny = 64, 64
-XMAX, YMAX = 40, 40
+XMAX, YMAX = ns.radius * 10, ns.radius * 10
 x = np.linspace(-XMAX, XMAX, nx)
 y = np.linspace(-YMAX, YMAX, ny)
 X, Y = np.meshgrid(x, y)
-r, theta = np.hypot(X, Y), np.arctan2(Y, X)
 
 coords = np.column_stack((X.flat, Y.flat, np.zeros(len(Y.flat))))
 t = 0
-
-# Magnetic field vector, B = (Ex, Ey), as separate components
-Br, Btheta = B(r, theta, np.radians(9.6))
-# Transform to Cartesian coordinates: NB make North point up, not to the right.
-c, s = np.cos(np.pi/2 + theta), np.sin(np.pi/2 + theta)
-Bx = -Btheta * s + Br * c
-By = Btheta * c + Br * s
+B_coords = ns.mag_field(t, coords)
+Bx = np.reshape(B_coords[:, 0], X.shape)
+By = np.reshape(B_coords[:, 1], X.shape)
 
 
 # Plot the streamlines with an appropriate colormap and arrow style
@@ -54,22 +37,17 @@ def init():
     ax.set_ylim(-YMAX, YMAX)
     ax.set_aspect('equal')
 
+    ax.add_patch(Circle((0,0), ns.radius, color='b', zorder=100))
 
     return ln.lines,
 
-def update(frame):
-    t = frame
-    alpha = np.radians(9.6) * np.cos(t)
-
+def update(t):
     ax.collections = [] # clear lines streamplot
     ax.patches = [] # clear arrowheads streamplot
     # Magnetic field vector, B = (Ex, Ey), as separate components
-    Br, Btheta = B(r, theta, alpha)
-    # Transform to Cartesian coordinates: NB make North point up, not to the right.
-    c, s = np.cos(np.pi/2 + theta), np.sin(np.pi/2 + theta)
-    Bx = -Btheta * s + Br * c
-    By = Btheta * c + Br * s
-
+    B_coords = ns.mag_field(t, coords)
+    Bx = np.reshape(B_coords[:, 0], X.shape)
+    By = np.reshape(B_coords[:, 1], X.shape)
 
     # Plot the streamlines with an appropriate colormap and arrow style
     color = 2 * np.log(np.hypot(Bx, By))
@@ -78,11 +56,11 @@ def update(frame):
 
 
     # Add a filled circle for the Earth; make sure it's on top of the streamlines.
-    ax.add_patch(Circle((0,0), RE, color='b', zorder=100))
+    ax.add_patch(Circle((0,0), ns.radius, color='b', zorder=100))
     return ln.lines,
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, np.pi, 16),
+ani = FuncAnimation(fig, update, frames=np.linspace(0, ns.ns_day, 16),
                     init_func=init, blit=True)
-ani.save('animation.mp4', writer='ffmpeg', fps=5)
+#ani.save('animation.mp4', writer='ffmpeg', fps=5)
 print("done saving")
 plt.show()
--- ns_field.py
+++ ns_field.py
@@ -14,36 +14,84 @@ class ns:
     def __init__(
             self,
             axis=np.array((0, 0, 1)),
-            radius=1e5,  #meters
-            dipole_moment=1e13 * np.array((0.1, 0, 1)),
-            quad_moment=1e12 * np.array((1, 0, 0)),
-            ns_day=0.1  #seconds
+            radius=1e4,  #meters
+            dipole_moment=1e30 * np.array((1, 0, 1)),
+            quad_moment=1e34 * np.array([[0, 0, 1], [0, 1, 0]]),
+            ns_day=1  #seconds
     ):
 
         self.axis = np.asarray(axis)
         self.radius = radius
         self.dipole_moment = np.asarray(dipole_moment)
+        # go ahead and make sure our quadrupole moment is of the right form
         self.quad_moment = np.asarray(quad_moment)
+        assert np.dot(quad_moment[0], quad_moment[1]) == 0
+        quad_moment[1] = quad_moment[1] / norm(quad_moment[1])
         self.ns_day = ns_day
 
     def mag_field(self, time, pos):
         pos = np.asarray(pos)
         # first compute the current moments
         rot_matrix = rotation_matrix(self.axis, 2 * np.pi * time / self.ns_day)
+        # rotate the dipole moment (easy)
         dmoment = np.dot(rot_matrix, self.dipole_moment)
-        qmoment = np.dot(rot_matrix, self.quad_moment)
-
-        # for a array of 3d vectors, this finds the norm of each
-        rnorm = np.sqrt(np.einsum('ij,ij->i', pos, pos))
+        # rotate the two vectors in the quadrupole moment
+        qmoment = np.copy(self.quad_moment)
+        qmoment[0] = np.dot(rot_matrix, self.quad_moment[0])
+        qmoment[1] = np.dot(rot_matrix, self.quad_moment[1])
         # next compute field from dipole moment
-        B_dip = ((sp.constants.mu_0 / (4 * np.pi))
-                * ((3 * pos * (np.einsum('ij,j->i', pos, dmoment)[:, np.newaxis] /
-                    (rnorm**5)[:, np.newaxis]))
-                     - (dmoment[:, np.newaxis] / rnorm**3).T))
-        return B_dip
-        # and the quadrupole field
+        B_dip = dipole(dmoment, pos)
+        # and the quadrupole moment
+        B_quad = quadrupole(qmoment, pos)
+        return B_dip + B_quad
+        # return B_dip
+        # return B_quad
+
+def dipole(dmoment, pos, dip_location=np.array([0., 0., 0.])):
+    if len(pos.shape) == 1:
+        pos = pos[np.newaxis]
+    pos = pos - dip_location
+    rnorm = norm(pos)
+    B_dip = ((sp.constants.mu_0 / (4 * np.pi))
+            * ((3 * pos * (np.einsum('ij,j->i', pos, dmoment)[:, np.newaxis] /
+                (rnorm**5)[:, np.newaxis]))
+                 - (dmoment[:, np.newaxis] / rnorm**3).T))
+    return B_dip 
+
+def quadrupole(qmoment, pos, quad_location=np.array([0., 0., 0.]), size=1):
+    """qmoment should be 3x2 array, like [[x0, y0, z0], [x1, y1, z1]]
+    The first vector is the axial vector of the quadrupole, and sets the
+    strangth of the quadrupole. The second vector sets the orthogonal axis on
+    which lie the two out-facing dipoles.
+    Size sets the distance from the cetner that each dipole is placed. Defaults
+    to one and should be left at that without a good reason I guess.
+    """
+    qmag = norm(qmoment[0])
+    # normalized axial vector
+    naxial = qmoment[0] / qmag
+
+    # these are unit vectors for the four dipole moments we will sum
+    A = qmoment[1]
+    B = - A
+    C = np.cross(naxial, A)
+    D = - C
+    # now compute the field
+    #import ipdb; ipdb.set_trace()
+    field = dipole(qmag * A, pos, A * size)
+    field += dipole(qmag * B, pos, B * size)
+    field += dipole(- qmag * C, pos, C * size)
+    field += dipole(- qmag * D, pos, D * size)
+
+    return field - quad_location
 
 
+def norm(vect):
+    if len(vect.shape) == 1:
+        return np.sqrt(np.sum(vect * vect))
+    else:
+        return np.sqrt(np.einsum('ij,ij->i', vect, vect))
+
+ 
 def rotation_matrix(axis, theta):
     """
     Return the rotation matrix associated with counterclockwise rotation about
